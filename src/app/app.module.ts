3302faa78da7662233d8cb54125f3aa5724bd813
Zbigniew Piernik <zbigniewpiernik@gmail.com>
2023-01-25 09:44:10 +0100

    Added rooting and main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { RankingComponent } from './ranking/ranking.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    RankingComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
